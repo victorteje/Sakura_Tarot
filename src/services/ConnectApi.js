748f8ec1e92580a360047afbf668fad0ec0f3a71
Daniel Crespo <dani.crespo.rodriguez@gmail.com>
2023-07-03 14:31:14 +0200

    Educational comments (promise mode)

--- src/services/ConnectApi.js
+++ src/services/ConnectApi.js
@@ -7,3 +7,15 @@ export default async function ConnectApi(cardId){
         console.log(error);
     }
 }
+
+/*
+const APIURL = https://6388b6e5a4bb27a7f78f96a5.mockapi.io/sakura-cards/
+
+async function getAllRequest(user) {
+    await axios.get(`${APIURL}${user}`)
+            .then((response) => {
+                    return response;
+            })
+            .catch((error) => console.log(error));
+}
+*/
