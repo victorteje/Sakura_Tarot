0b9bf3f1877326d8388a8abcc18efd1252d3ebc0
Daniel Crespo <dani.crespo.rodriguez@gmail.com>
2023-07-03 15:20:44 +0200

    Axios working

---
+++ src/views/GeneralView.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { ref } from "vue";
+import ConnectApi from "../services/ConnectApi.js"
+
+const response = ref();
+
+response.value=ConnectApi(6);
+(response.value).then(x=>response.value=x)
+
+
+</script>
+
+<template>
+    <div>        
+        <p>{{ response.spanishName }}</p>
+ 
+ </div>
+</template>
+
+<style scoped>
+
+</style>
