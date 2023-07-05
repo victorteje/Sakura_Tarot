680767b0fce2fceb319c690dda0be990aca14352
Daniel Crespo <dani.crespo.rodriguez@gmail.com>
2023-07-05 08:50:43 +0200

    work of tuesday

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
-
+import CardComponent from './components/CardComponent.vue';
 </script>
 
 
 <template>
 <h1>Hola Frikis</h1>
+
+<CardComponent/>
 
 </template>
---
+++ src/components/CardComponent.vue
@@ -0,0 +1,35 @@
+<script setup>
+import { onBeforeMount } from 'vue';
+import ConnectApi from '../services/ConnectApi';
+
+let props=defineProps({
+    arrayOfChosen:{
+        type: Array
+    }
+});
+
+
+const array= ref([]);
+
+array.value=props.arrayOfChosen;
+
+
+</script>
+
+<template>
+<div>
+    <img src="../assets/images/Reverso-Clow.jpg" alt="">
+
+    
+</div>
+</template>
+
+<style scoped>
+
+div{
+    margin: 0 auto;
+    border: 2px solid white;
+    border-radius: 5px;
+}
+
+</style>
